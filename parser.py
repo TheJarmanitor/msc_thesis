e330b1e04d9d8a14c587ea1608cb821ea6e30cd0
Oleg Jarma Montoya <ojarmam@unal.edu.co>
2025-05-04 18:55:27 +0200

    update parser

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 # %%
 import numpy as np
 from pathlib import Path
+import pandas as pd
 # %%
 logs_folder = ".logs"
 logs_path = Path(logs_folder)
@@ -15,15 +16,23 @@ sessions_load = [
 
 session_data = [load.f.arr_0 for load in sessions_load]
 # %%
+df = pd.DataFrame()
 for session in session_data:
     game_states = np.array([frame["obs_tp1"]["state"] for frame in session])
+    state_names = [f"ram_{i}" for i in range(game_states.shape[1])]
     game_actions = np.array([frame["action"] for frame in session])
     game_mode = session[0]["game_mode"]
     game_difficulty = session[0]["game_difficulty"]
     participant_id = session[0]["participant_id"]
-    print(range(len(session)))
+    trial = session[0]["trial_number"]
+    temp_df = pd.DataFrame(game_states, columns=state_names)
+    temp_df["action"] = game_actions
+    temp_df["mode"] = game_mode
+    temp_df["difficulty"] = game_difficulty
+    temp_df["participant_id"] = participant_id
+    temp_df["trial"] = trial
 
-# for i in range(len(game_states)):
-#     print(game_actions[i])
-# print(np.max(game_states[:, 105:109], axis=0))
+    df = pd.concat([df, temp_df])
+# %%
+
 # %%
